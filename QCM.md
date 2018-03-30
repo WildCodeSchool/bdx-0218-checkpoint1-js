67149c902ba713ee93a416500549a0a7518387b1
Coralie.brtl <coralie.brt@live.fr>
2018-03-30 11:45:14 +0200

    Coralie

--- QCM.md
+++ QCM.md
@@ -1,12 +1,12 @@
 ## Reponses au QCM (séparé par des virgules quand il y a plusieurs choix de réponses)
 
-* 1.
-* 2.
-* 3.
-* 4.
-* 5.
-* 6.
-* 7.
-* 8.
-* 9.
-* 10.
+* 1. e/ master
+* 2. d/ git checkout "X"
+* 3. a/ git clone
+* 4. b/ npm init
+* 5. a/ added, b/ modified, f/ unmodified
+* 6. b/ gid add -> git commit -> git push
+* 7. c/ nano
+* 8. b/ C'est une pull request, d/ Cela permet de ne pas merger tout de suite sur la master, e/ Cela permet de faire de la review de code
+* 9. b/ objet[i]
+* 10. c/ git status
--- checkpoint.js
+++ checkpoint.js
@@ -1,5 +1,45 @@
-const bulletin = {};
+const bulletin = {nom_partie : 'Partie 1',
+                 notes : new Array() ,
+                 points : 0 ,
+                 eliminer : false,
+                 moyenne : false,
+                 };
 
+function add_property (){
+    for (let i=0; i<10; i++){
+        bulletin.notes[i] = Math.floor(Math.random()*20);
+        if (bulletin.notes[i]>10){
+            bulletin.points ++
+        }
+        else if (bulletin.notes[i]===0){
+            eliminer = true;
+        }
+    }
+};
+
+function add_moyenne (){
+    for (let j=0; j<bulletin.notes.length; j++){
+        bulletin.moyenne= bulletin.moyenne+ bulletin.notes[j];
+    }
+    bulletin.moyenne= bulletin.moyenne/bulletin.notes.length
+}
+;
+add_property();
+add_moyenne();
+
+bulletin.reset = false
+
+
+function add_reset(){
+    bulletin.reset=true;
+    if (bulletin.reset=true) {
+        bulletin.notes = new Array(),
+        bulletin.points = 0 ,
+        bulletin.eliminer = false,
+        bulletin.moyenne = false
+    }
+};
+    
 console.log(bulletin);
 // Complete le bulletin
 // Your code here... :)
