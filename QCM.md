2a43a3522c3aa794b246a5faa84aa51167a1d9c3
Maco <maco.dou@hotmail.com>
2018-03-30 11:57:57 +0200

    Checkpoint Checked

--- QCM.md
+++ QCM.md
@@ -1,12 +1,12 @@
 ## Reponses au QCM (séparé par des virgules quand il y a plusieurs choix de réponses)
 
-* 1.
-* 2.
-* 3.
-* 4.
-* 5.
-* 6.
-* 7.
-* 8.
-* 9.
-* 10.
+* 1. e/ Master
+* 2. d/ git checkout "X
+* 3. a/ git clone
+* 4. b/ npm init
+* 5. c/ Modified, e/ Staged, f/ Unmodified
+* 6. b/ git add -> git commit -> git push
+* 7. b/ edit, c/ nano, d/ vi
+* 8. b/ C'est une pull request, c/ cela permet de travailler sur sa branche, d/ cela permet de ne pas merger tout de suite sur la master, e/ cela permet de faire de la review de code, f/ c'est une étape de workflow
+* 9. d/ objet['propriete']
+* 10. c/ git status
--- checkpoint.js
+++ checkpoint.js
@@ -1,8 +1,46 @@
-const bulletin = {};
+const bulletin = {
+    nom : "Partie 1",
+    notes : [],
+    points : 0,
+    eliminer : false,
+    moyenne : false,
+    reset() {
+         return () => {
+              this.nom = "Partie 1";
+              this.notes = [];
+              this.points = 0;
+              this.eliminer = false;
+              this.moyenne = false;
+         }
+    }
+}
 
-console.log(bulletin);
+console.log();
+
 // Complete le bulletin
-// Your code here... :)
+
+
+
+
+for (let i = 0; i < 10; i++) {
+    bulletin.notes[i] = Math.round(Math.random() * 20);
+}notes = [Math.floor(Math.random() * Math.floor(20)), Math.floor(Math.random() * Math.floor(20)), Math.floor(Math.random() * Math.floor(20)), Math.floor(Math.random() * Math.floor(20)), Math.floor(Math.random() * Math.floor(20)), Math.floor(Math.random() * Math.floor(20)), Math.floor(Math.random() * Math.floor(20)), Math.floor(Math.random() * Math.floor(20)), Math.floor(Math.random() * Math.floor(20)), Math.floor(Math.random() * Math.floor(20))];
+
+for (i = 0; i < bulletin.notes.length; i++) {
+    if (bulletin.notes[i] >= 10) {
+        bulletin.points += 1;
+    } else if (bulletin.notes[i] == 0) {
+        return eliminer = true;
+    } else {
+        bulletin.points += 0;
+    }
+};
+
+for(i = 0; i < bulletin.notes.length; i++){
+    bulletin.moyenne += bulletin.notes[i] / bulletin.notes.length;
+};
+
+
 
 // Don't touch this...
 module.export = bulletin;
