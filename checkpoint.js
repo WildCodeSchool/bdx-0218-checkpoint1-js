86aa01669afee952db829c20402d9f41b8bf0ed7
Fanny Gaudin <mauriceleptitpoissonrouge@hotmail.fr>
2018-03-30 09:58:53 +0200

    QCM - solved checkpoint en cours

--- checkpoint.js
+++ checkpoint.js
@@ -1,8 +1,18 @@
-const bulletin = {};
+const bulletin = {
+	'nom de la partie':'Partie 1',
+	'notes':[],
+	'points':0,
+	'eliminer':false,
+	'moyenne':false
+};
 
 console.log(bulletin);
 // Complete le bulletin
 // Your code here... :)
+const notesBull = (_arg) => {
+	let notes = _arg.notes;
+	console.log(notes);
+}
 
 // Don't touch this...
 module.export = bulletin;
