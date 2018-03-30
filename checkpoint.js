d8110fe1e17e13df240234e97cabf20f5025a5cf
Marion Ruat <marionruat@hotmail.com>
2018-03-30 12:14:09 +0200

    Checkpoints

--- checkpoint.js
+++ checkpoint.js
@@ -1,8 +1,36 @@
-const bulletin = {};
+const bulletin = {
+	nom_de_la_partie: 'Partie 1',
+	notes: [],
+	points:0,
+	eliminer: false,
+	moyenne:false,
+};
 
-console.log(bulletin);
-// Complete le bulletin
-// Your code here... :)
+var n = 0;
+while ( n < 10 ) {
+	n++;
+	bulletin.notes.push(Math.floor(Math.random() * (21 - 0 + 1)) + 0)
+		}
 
-// Don't touch this...
-module.export = bulletin;
+function nbpoints (t) {
+	points=0
+		if (bulletin.notes>10){
+			points=points+1;
+			return points;
+	}
+}
+
+console.log(bulletin.notes);
+console.log(nbpoints);
+
+
+// console.log(points);
+
+// console.log(entierAleatoire)
+
+// console.log(bulletin);
+// // Complete le bulletin
+// // Your code here... :)
+
+// // Don't touch this...
+// module.export = bulletin;
