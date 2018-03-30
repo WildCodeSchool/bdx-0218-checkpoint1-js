b1bda42623635f366d15b37c7a4ca3baa5181893
Caroline Collery <caro.collery@gmail.com>
2018-03-30 12:13:56 +0200

    retour

--- QCM.md
+++ QCM.md
@@ -1,12 +1,12 @@
 ## Reponses au QCM (séparé par des virgules quand il y a plusieurs choix de réponses)
 
-* 1.
-* 2.
-* 3.
-* 4.
-* 5.
-* 6.
-* 7.
-* 8.
-* 9.
-* 10.
+* 1. e
+* 2. c
+* 3. a
+* 4. b
+* 5. a
+* 6. b
+* 7. c
+* 8. b, d
+* 9. e
+* 10.c
--- checkpoint.js
+++ checkpoint.js
@@ -1,8 +1,21 @@
-const bulletin = {};
+const bulletin = {
+	nom : "Partie 1",
+	notes : [],
+	points : 0,
+	eliminer : false,
+	moyenne : false
+};
+// console.log(bulletin);
+// // Complete le bulletin
+// // Your code here... :)
+// //etape 2 :
+function add (note) {
+	bulletin.notes.push().Math.floor(20);
+}
+add(10);	
+console.log(bulletin.notes);
+// var randomValue = bulletin.notes[Math.floor(bulletin.notes.length * Math.random(10))];
 
-console.log(bulletin);
-// Complete le bulletin
-// Your code here... :)
-
+// //etape 3
 // Don't touch this...
 module.export = bulletin;
