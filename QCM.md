6c92cb7e4466be3a1d8e012f831b76803291a652
AlexLoWCD <alexandre.lorion@wildcodeschool.fr>
2018-04-04 10:04:04 +0200

    Correction Checkpoint

--- QCM.md
+++ QCM.md
@@ -1,12 +1,12 @@
 ## Reponses au QCM (séparé par des virgules quand il y a plusieurs choix de réponses)
 
-* 1.
-* 2.
-* 3.
-* 4.
-* 5.
-* 6.
-* 7.
-* 8.
-* 9.
-* 10.
+* 1. master
+* 2. git checkout -b "X" / git checkout "X"
+* 3. git clone
+* 4. npm init
+* 5. Modified / Staged / Unmodified
+* 6. git add -> git commit -> git push
+* 7. nano / vi
+* 8. C'est une pull request / cela permet de faire de la review de code / c'est une étape de workflow
+* 9. objet.propriete / objet['propriete']
+* 10. git status
--- checkpoint.js
+++ checkpoint.js
@@ -1,8 +1,49 @@
-const bulletin = {};
+const bulletin = {
+  partyName: 'Partie 1',
+  grades: [],
+  points: 0,
+  eliminate: false,
+  average: false,
+  reset: function() {
+    this.partyName = 'Partie 1';
+    this.grades = [];
+    this.points = 0;
+    this.eliminate = false;
+    this.average = false;
+  }
+};
 
-console.log(bulletin);
 // Complete le bulletin
-// Your code here... :)
+
+for (let i = 0; i < 10; i++) {
+  bulletin.grades.push(
+    Math.floor(Math.random() * 20)
+  );
+}
+
+for (let i = 0; i < bulletin.grades.length; i++) {
+  const grade = bulletin.grades[i];
+  if (grade > 10) {
+    bulletin.points++;
+  }
+}
+
+for (let i = 0; i < bulletin.grades.length; i++) {
+  const grade = bulletin.grades[i];
+  if (grade === 0) {
+    bulletin.eliminate = true;
+  }
+}
+
+let sum = 0;
+for (let i = 0; i < bulletin.grades.length; i++) {
+  const grade = bulletin.grades[i];
+  sum += grade;
+}
+
+bulletin.average = sum / bulletin.grades.length;
+
+bulletin.reset();
 
 // Don't touch this...
 module.export = bulletin;
